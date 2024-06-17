ea8800a4a6c99844ddba8eb5a3fdce8caf134f03
srmeier <info@stephenmeier.net>
2024-06-17 16:30:43 -0400

    Adding my custom mods

--- train_gpt2.py
+++ train_gpt2.py
@@ -322,7 +322,7 @@ if torch.cuda.is_available():
 enc = tiktoken.get_encoding("gpt2")
 
 total_batch_size = 524288 # 2**19, ~0.5M, in number of tokens
-B = 64 # micro batch size
+B = int(64 / 4) # micro batch size (dividing by 4 in order to fit on my RTX 4090)
 T = 1024 # sequence length
 assert total_batch_size % (B * T * ddp_world_size) == 0, "make sure total_batch_size is divisible by B * T * ddp_world_size"
 grad_accum_steps = total_batch_size // (B * T * ddp_world_size)
